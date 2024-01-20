0ff4dec07541883ddd14a19c7086525761e483a3
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2024-01-20 09:52:21 +0530

    Add files via upload

---
+++ January/Distribute_candies_in_a_binary_tree.java
@@ -0,0 +1,20 @@
+class Distribute_candies_in_a_binary_tree {
+    static int moves;
+
+    static int solve(Node root) {
+        if (root == null)
+            return 0;
+
+        int ln = solve(root.left);
+        int rn = solve(root.right);
+
+        moves += Math.abs(ln) + Math.abs(rn);
+        return root.data - 1 + ln + rn;
+    }
+
+    public static int distributeCandy(Node root) {
+        moves = 0;
+        solve(root);
+        return moves;
+    }
+}
