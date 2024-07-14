55a195cc51b84500ee8bcb1eac6924c4466e898e
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2024-07-14 19:43:02 +0530

    Add files via upload

---
+++ July/Segregate_0s_and_1s.java
@@ -0,0 +1,14 @@
+package July;
+
+import java.util.*;
+
+public class Segregate_0s_and_1s {
+     void segregate0and1(int[] arr) {
+          // code here
+          Arrays.sort(arr);
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
