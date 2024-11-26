cae7b51ecb9fa7c01fd6c80c241b2c3c956fa2b9
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2024-11-26 14:08:36 +0530

    Add files via upload

---
+++ November/Find_Champion.java
@@ -0,0 +1,30 @@
+package November;
+
+public class Find_Champion {
+     public int findChampion(int n, int[][] edges) {
+          boolean[] isUndefeated = new boolean[n];
+          Arrays.fill(isUndefeated, true);
+
+          for (int[] edge : edges) {
+               int winner = edge[0];
+               int loser = edge[1];
+               isUndefeated[loser] = false;
+          }
+
+          int champion = -1;
+          int championCount = 0;
+
+          for (int team = 0; team < n; team++) {
+               if (isUndefeated[team]) {
+                    champion = team;
+                    championCount++;
+               }
+          }
+
+          return championCount == 1 ? champion : -1;
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
