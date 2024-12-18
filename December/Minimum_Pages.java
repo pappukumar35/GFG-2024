249ca044363f8fba31cd3b038353ed3b7062c793
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2024-12-18 13:34:02 +0530

    Add files via upload

---
+++ December/Minimum_Pages.java
@@ -0,0 +1,51 @@
+package December;
+
+public class Minimum_Pages {
+     public static int countStudent(int[] arr, int pages) {
+          int n = arr.length;
+          int student = 1;
+          long studentPages = 0;
+          for (int i = 0; i < n; i++) {
+               if (studentPages + arr[i] <= pages) {
+                    studentPages += arr[i];
+               } else {
+                    student++;
+                    studentPages = arr[i];
+               }
+          }
+          return student;
+     }
+
+     public static int findPages(int[] arr, int k) {
+          int n = arr.length;
+          if (k > n) {
+               return -1;
+          }
+          int low = arr[0];
+
+          for (int i = 1; i < arr.length; i++) {
+               if (arr[i] > low) {
+                    low = arr[i];
+               }
+          }
+          int high = 0;
+          for (int i = 0; i < n; i++) {
+               high += arr[i];
+
+          }
+          while (low <= high) {
+               int mid = (low + high) / 2;
+               if (countStudent(arr, mid) > k) {
+                    low = mid + 1;
+               } else {
+                    high = mid - 1;
+               }
+          }
+          return low;
+          // code here
+     }
+
+     public static void main(String[] args) {
+
+     }
+}
